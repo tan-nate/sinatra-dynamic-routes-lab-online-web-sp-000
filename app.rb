55c040e04154d34881ada1da855c5cbbe7d0463e
Nate Tan <nktan93@gmail.com>
2019-05-29 16:44:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get 'reversename/:name' do
-    name = params[:name]
-    reversename = name.reverse!
+    @name = params[:name]
+    @reversename = name.reverse!
     "reversename"
 end
