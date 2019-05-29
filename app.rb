76e2a90360a95f3007f1c0cf0cedcdb95fc784ab
Nate Tan <nktan93@gmail.com>
2019-05-29 17:46:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    @eval = "#{@number1} #{@operation} #{@number2}".eval
+    @eval = eval "#{@number1} #{@operation} #{@number2}"
     "#{@eval}"
     binding.pry
   end
