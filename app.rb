ae9febac120df3403b2e979242b8f6103af25339
Nate Tan <nktan93@gmail.com>
2019-05-29 17:28:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,15 @@ class App < Sinatra::Base
     end
     @phrase_repeat = @phrase_array.join(" ")
     "#{@phrase_repeat}"
-    #binding.pry
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1 + @word2 + @word3 + @word4 + @word5}"
   end
 
 end
